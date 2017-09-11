dc5751395c255f2956509dfcd7e5628aa158e841
Lisa The <lisathe97@gmail.com>
2017-09-11 11:09:20 +0200

    Draw Line - Cubeys not visible

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public GameObject lineObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //  Vector3 _objectPos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 _objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Ray ray = Camera.main.ScreenP
+            Instantiate(lineObj, _objectPos, Quaternion.identity);
+            
+
+        }
+
+		
+	}
+}
