23f4fda332036b2e0485edb77e16df292c578cb1
Lisa The <lisathe97@gmail.com>
2017-09-11 11:31:51 +0200

    Draw Line finished :D

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -12,17 +12,16 @@ public class DrawLine : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        if (Input.GetMouseButtonDown(0))
+	void Update () {
+      //  if (Input.GetMouseButtonDown(0))
+      if(Input.anyKey)
         {
             //  Vector3 _objectPos = Camera.current.ScreenToWorldPoint(Input.mousePosition);
             Vector3 _objectPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //Ray ray = Camera.main.ScreenP
-            Instantiate(lineObj, _objectPos, Quaternion.identity);
-            
+            Instantiate(lineObj, new Vector3(_objectPos.x, _objectPos.y, _objectPos.z+1), Quaternion.Euler(0,180,0));
 
         }
 
 		
-	}
+	} 
 }
