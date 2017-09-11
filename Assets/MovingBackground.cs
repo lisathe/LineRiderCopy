2ee2ecf910fd0616ab25696a319a99efcea43d1c
Lisa The <lisathe97@gmail.com>
2017-09-11 15:01:59 +0200

    Scrolling background-  Attempt 1

---
+++ Assets/MovingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour {
+
+    public float scrollSpeed;
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+    }
+
+    void Scroll()
+    {
+        //Moving horizontally
+        Vector2 offset = new Vector2(Time.time * scrollSpeed, 0);
+        rend.material.mainTextureOffset = offset;
+    }
+}
